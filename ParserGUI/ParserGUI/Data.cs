89e18d928bf34ae0907c63b3f5d46ba69f2ab78b
lexapech <lexa.pech@yandex.ru>
2023-04-04 22:00:02 +0300

    added data class

---
+++ ParserGUI/ParserGUI/Data.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParserCore
+{
+    public class Data
+    {
+        public struct Parameter
+        {
+            public string Name;
+            public string Range;
+            public string Description;
+        
+        }
+
+        private List<Parameter> _parameters = new List<Parameter>();
+
+        public List<Parameter> ReadAll()
+        {
+            return _parameters;
+        }
+        public Parameter ReadElem(int index)
+        {
+            return _parameters[index];
+        }
+        public void WriteElem(Parameter element)
+        {
+            _parameters.Add(element);
+        }
+
+    }
+}
