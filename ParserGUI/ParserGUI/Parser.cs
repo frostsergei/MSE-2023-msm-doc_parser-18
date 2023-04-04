0fba72a2719de915ff55ba6e7af1e6f42543d458
lexapech <lexa.pech@yandex.ru>
2023-04-04 22:14:02 +0300

    move document content parser into parser class

--- ParserGUI/ParserGUI/Parser.cs
+++ ParserGUI/ParserGUI/Parser.cs
@@ -2,12 +2,12 @@
 using UglyToad.PdfPig.Content;
 using System.Collections.Generic;
 using Tabula;
-using ParserGUI;
+using UglyToad.PdfPig.DocumentLayoutAnalysis.WordExtractor;
 using System.IO;
-using System;
 using System.Xml;
+using System;
 using System.Linq;
-using static UglyToad.PdfPig.Core.PdfSubpath;
+using System.Text;
 
 namespace ParserCore
 {
@@ -84,6 +84,381 @@ namespace ParserCore
             str.Position = 0;
             StreamReader str_rd = new StreamReader(str);
             return str_rd.ReadToEnd();
+        }
+
+
+        private class TableColumn
+        {
+            public double left;
+            public List<Word> words = new List<Word>();
+        }
+
+        private class WordComparer : IComparer<Word>
+        {
+            public int Compare(Word word1, Word word2)
+            {
+                double top_margin = (word1.BoundingBox.Height + word2.BoundingBox.Height) / 2;
+                if (object.ReferenceEquals(word1, word2))
+                    return 0;
+                else
+                {
+                    if (Math.Abs(word1.BoundingBox.Top - word2.BoundingBox.Top) <= top_margin)
+                        return word1.BoundingBox.Left < word2.BoundingBox.Left ? -1 :
+                            word1.BoundingBox.Left > word2.BoundingBox.Left ? 1 : 0;
+                    return word1.BoundingBox.Top < word2.BoundingBox.Top ? 1 :
+                            word1.BoundingBox.Top > word2.BoundingBox.Top ? -1 : 0;
+                }
+            }
+        }
+
+        public string ParseStringParams(List<int> pages_numbers, string doc_name)
+        {
+            const double column_margin = 10;
+            MemoryStream str = new MemoryStream();
+            XmlWriterSettings settings = new XmlWriterSettings();
+            settings.Indent = true; settings.IndentChars = "\t";
+            XmlWriter writer = XmlWriter.Create(str, settings);
+
+            writer.WriteStartElement("TagList");
+            foreach (int page_num in pages_numbers)
+            {
+                PdfDocument doc = PdfDocument.Open(doc_name);
+                IEnumerable<Word> raw_words = doc.GetPage(page_num).GetWords();
+                List<Word> words = new List<Word>(raw_words);
+                words.Sort(new WordComparer());
+                List<TableColumn> table_columns = new List<TableColumn>();
+                bool parse = false;
+                foreach (Word w in words)
+                {
+                    if (w.FontName.ToLower().Contains("bold"))
+                        parse = true;
+                    if (!parse)
+                        continue;
+                    bool create_new_col = true;
+                    foreach (TableColumn column in table_columns)
+                    {
+                        if (Math.Abs(w.BoundingBox.Left - column.left) <= column_margin)
+                        {
+                            column.words.Add(w);
+                            create_new_col = false;
+                            break;
+                        }
+                    }
+                    if (create_new_col)
+                    {
+                        var new_col = new TableColumn();
+                        new_col.left = w.BoundingBox.Left;
+                        new_col.words.Add(w);
+                        table_columns.Add(new_col);
+                    }
+                }
+                for (int i = 2; i < table_columns.Count; i++)
+                {
+                    table_columns[1].words.AddRange(table_columns[i].words);
+                }
+                table_columns.RemoveRange(2, table_columns.Count - 2);
+                table_columns[1].words.Sort(new WordComparer());
+                int column_1_i = 0, column_2_i = 0;
+                double line_height_1 = table_columns[0].words[0].BoundingBox.Height;
+                double line_height_2 = table_columns[1].words[0].BoundingBox.Height;
+                List<List<TableColumn>> rows = new List<List<TableColumn>>();
+                while (column_1_i < table_columns[0].words.Count && column_2_i < table_columns[1].words.Count)
+                {
+                    List<TableColumn> row = new List<TableColumn>();
+                    TableColumn tc1 = new TableColumn(), tc2 = new TableColumn();
+                    double last_y = table_columns[0].words[column_1_i].BoundingBox.Top;
+                    for (; column_1_i < table_columns[0].words.Count
+                        && table_columns[0].words[column_1_i].BoundingBox.Top >= last_y - line_height_1 * 2; column_1_i++)
+                    {
+                        if (!table_columns[0].words[column_1_i].FontName.ToLower().Contains("bold"))
+                        {
+                            goto end_table;
+                        }
+                        if (table_columns[0].words[column_1_i].BoundingBox.Top <= last_y)
+                        {
+                            last_y = table_columns[0].words[column_1_i].BoundingBox.Top;
+                        }
+                        tc1.words.Add(table_columns[0].words[column_1_i]);
+                    }
+                    double last_y_2 = table_columns[1].words[column_2_i].BoundingBox.Top;
+                    for (; column_2_i < table_columns[1].words.Count
+                        && table_columns[1].words[column_2_i].BoundingBox.Top >= last_y_2 - line_height_2 * 2; column_2_i++)
+                    {
+                        if (table_columns[1].words[column_2_i].BoundingBox.Top <= last_y_2)
+                        {
+                            last_y_2 = table_columns[1].words[column_2_i].BoundingBox.Top;
+                        }
+                        tc2.words.Add(table_columns[1].words[column_2_i]);
+                    }
+                    row.Add(tc1);
+                    row.Add(tc2);
+                    rows.Add(row);
+                }
+            end_table:
+                foreach (List<TableColumn> row in rows)
+                {
+                    string description = "";
+                    foreach (Word w in row[1].words)
+                    {
+                        description += w.Text + ' ';
+                    }
+                    foreach (Word w in row[0].words)
+                    {
+                        string param = w.Text.Replace(",", "");
+                        writer.WriteStartElement("Row");
+                        writer.WriteStartAttribute("Param");
+                        writer.WriteString(param);
+                        writer.WriteEndAttribute();
+                        writer.WriteStartAttribute("Description");
+                        writer.WriteString(description);
+                        writer.WriteEndAttribute();
+                        writer.WriteEndElement();
+                    }
+                }
+            }
+            writer.WriteEndElement();
+            writer.Flush();
+            str.Position = 0;
+            StreamReader str_rd = new StreamReader(str);
+            return str_rd.ReadToEnd();
+        }
+
+        private class DocumentContentParser
+        {
+            struct ContentTableItem
+            {
+                public string Number;
+                public string Title;
+                public int Page;
+            }
+
+
+            private PdfDocument _document;
+            private List<string> _parsedLines;
+            private static string _contentTableTitle = "содержание";
+            private static int _maxPageNumber = 10;
+            private int _pageNumberOffset = 0;
+            private List<ContentTableItem> _contentItems;
+            public DocumentContentParser(PdfDocument document)
+            {
+                _document = document;
+                _parsedLines = new List<string>();
+            }
+            public List<int> Parse()
+            {
+                var contentPageNumber = FindContentPage();
+                ParseAllContent(contentPageNumber);
+                _contentItems = _parsedLines.Select(line => ParseContentTableItem(line)).ToList();
+                return GetPages();
+            }
+
+            private int FindContentPage()
+            {
+                var pageNumber = 1;
+                while (pageNumber < _maxPageNumber)
+                {
+                    var pageLines = GetPageLines(pageNumber);
+                    bool found = false;
+                    for (int i = 0; i < pageLines.Count; i++)
+                    {
+                        if (pageLines[i].ToLower().Contains(_contentTableTitle))
+                        {
+                            _pageNumberOffset = pageNumber - ParsePageNumber(pageLines[0]);
+                            _parsedLines.AddRange(pageLines.Where((line, index) => index > i));
+                            found = true;
+                            break;
+                        }
+                    }
+                    if (found) break;
+                    pageNumber++;
+                }
+                return pageNumber;
+            }
+
+            private int ParsePageNumber(string line)
+            {
+                var words = line.Split(' ').ToList();
+                var firstWord = words.First(word => !string.IsNullOrWhiteSpace(word));
+                int pageNumber;
+                if (int.TryParse(firstWord, out int _))
+                {
+                    pageNumber = int.Parse(firstWord);
+                }
+                else
+                {
+                    var lastWord = words.Last(word => !string.IsNullOrWhiteSpace(word) && int.TryParse(word, out int _));
+                    if (lastWord == null) throw new Exception("Cannot find page number");
+                    pageNumber = int.Parse(lastWord);
+                }
+                return pageNumber;
+            }
+
+            private int GetRealPageNumber(int documentPageNumber)
+            {
+                return documentPageNumber + _pageNumberOffset;
+            }
+
+            private ContentTableItem ParseContentTableItem(string line)
+            {
+                var words = line.Split(' ').ToList();
+                ContentTableItem item = new ContentTableItem()
+                {
+                    Number = char.IsDigit(words[0][0]) ? words[0] : "",
+                    Title = string.Join(" ",
+                            words.Where(word => !string.IsNullOrWhiteSpace(word) &&
+                                                !char.IsDigit(word[0]) &&
+                                                 word.Count(x => x == '.') < 3
+                                                 )
+                            ),
+                    Page = GetRealPageNumber(int.Parse(words.Last(word => !string.IsNullOrWhiteSpace(word)).Replace(".", "")))
+                };
+                return item;
+            }
+
+            private List<string> GetPageLines(int pageNumber)
+            {
+                var page = _document.GetPage(pageNumber);
+                var text = GetText((List<Letter>)page.Letters);
+                var lines = text.Split('\n')
+                            .Select(line => line.Replace("\r", ""))
+                            .Where(line => !string.IsNullOrWhiteSpace(line))
+                            .ToList();
+                return lines;
+            }
+
+            private void ParseAllContent(int firstPage)
+            {
+                var firstEntryPage = ParseContentTableItem(_parsedLines[0]).Page;
+                for (int i = firstPage + 1; i < firstEntryPage; i++)
+                {
+                    _parsedLines.AddRange(GetPageLines(i).Where((line, index) => index > 0));
+                }
+            }
+
+            private ContentTableItem GetNextOnTheSameLevel(ContentTableItem item)
+            {
+                var digits = item.Number.Split('.').Length;
+                var currentIndex = _contentItems.IndexOf(item);
+                return _contentItems.FirstOrDefault((listItem) =>
+                {
+                    var listDigits = listItem.Number.Split('.').Length;
+                    return _contentItems.IndexOf(listItem) > currentIndex && listDigits == digits;
+                });
+            }
+
+            private List<int> GetPages()
+            {
+                string[] whiteList = new string[] { "ТЕК", "НАСТР", "БД", "Общесистем", "Вычисляемые", "вычисляемые", "параметр", "настроечн", "Настроечн", "Текущ" };
+                string[] blackList = new string[] { "Приложение", "списки", "список", "Списки", "Список", "контр", "Контр", "Структур", "Ввод", "Режим", "режим", "справка", "Справка" };
+
+                var pages = new SortedSet<int>();
+                foreach (var item in _contentItems)
+                {
+                    if (whiteList.Any(word => item.Title.Contains(word)) &&
+                    !blackList.Any(word => item.Title.Contains(word)))
+                    {
+                        var startPage = item.Page;
+                        var endPage = GetNextOnTheSameLevel(item).Page;
+                        for (int j = startPage; j <= endPage; j++)
+                        {
+                            pages.Add(j);
+                        }
+                    }
+                }
+                foreach (var item in _contentItems)
+                {
+                    if (blackList.Any(word => item.Title.Contains(word)))
+                    {
+                        var startPage = item.Page;
+                        var prevIndex = _contentItems.IndexOf(item) - 1;
+                        bool keepFirstPage = true;
+                        if (prevIndex >= 0 && _contentItems[prevIndex].Number.Split('.').Length < item.Number.Split('.').Length)
+                        {
+                            keepFirstPage = false;
+                        }
+                        int endPage;
+                        if (_contentItems.IndexOf(item) + 1 >= _contentItems.Count)
+                        {
+                            endPage = _document.NumberOfPages;
+                        }
+                        else
+                        {
+                            endPage = _contentItems[_contentItems.IndexOf(item) + 1].Page;
+                        }
+
+                        for (int j = startPage + (keepFirstPage ? 1 : 0); j < endPage; j++)
+                        {
+                            pages.Remove(j);
+                        }
+                    }
+                }
+                return pages.ToList();
+            }
+
+            private string GetText(List<Letter> letters)
+            {
+                StringBuilder stringBuilder = new StringBuilder();
+                var wordExtractorOptions = new NearestNeighbourWordExtractor.NearestNeighbourWordExtractorOptions()
+                {
+                    Filter = (pivot, candidate) =>
+                    {
+                        if (string.IsNullOrWhiteSpace(candidate.Value))
+                        {
+                            return false;
+                        }
+
+                        var maxHeight = Math.Max(pivot.PointSize, candidate.PointSize);
+                        var minHeight = Math.Min(pivot.PointSize, candidate.PointSize);
+                        if (minHeight != 0 && maxHeight / minHeight > 2.0)
+                        {
+                            return false;
+                        }
+                        var pivotRgb = pivot.Color.ToRGBValues();
+                        var candidateRgb = candidate.Color.ToRGBValues();
+                        if (!pivotRgb.Equals(candidateRgb))
+                        {
+                            return false;
+                        }
+                        return true;
+                    }
+                };
+                var wordExtractor = new NearestNeighbourWordExtractor(wordExtractorOptions);
+
+                var words = wordExtractor.GetWords(letters);
+
+                Word previous = null;
+                foreach (var word in words)
+                {
+                    if (previous != null)
+                    {
+                        var hasInsertedWhitespace = false;
+                        var bothNonEmpty = previous.Letters.Count > 0 && word.Letters.Count > 0;
+                        if (bothNonEmpty)
+                        {
+                            var prevLetter1 = previous.Letters[0];
+                            var currentLetter1 = word.Letters[0];
+
+                            var baselineGap = Math.Abs(prevLetter1.StartBaseLine.Y - currentLetter1.StartBaseLine.Y);
+
+                            if (baselineGap > 3)
+                            {
+                                hasInsertedWhitespace = true;
+                                stringBuilder.AppendLine();
+                            }
+                        }
+
+                        if (!hasInsertedWhitespace)
+                        {
+                            //stringBuilder.Append(" ");
+                        }
+                    }
+
+                    stringBuilder.Append(word.Text);
+
+                    previous = word;
+                }
+                return stringBuilder.ToString();
+            }
         }
     }
 }
